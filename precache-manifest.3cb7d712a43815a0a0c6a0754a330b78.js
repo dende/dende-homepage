7714b5fc992352795530d804dea2a2f9883a4fd0
Christian Hartlage <c@dende.de>
2021-07-30 23:33:51 +0200

    Updates

---
+++ precache-manifest.3cb7d712a43815a0a0c6a0754a330b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f9f991561487813565b45385dabd40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bceaaccfbdaaf620cdd",
+    "url": "/static/css/2.77d6c709.chunk.css"
+  },
+  {
+    "revision": "31c507f77c4d05a8c752",
+    "url": "/static/css/main.e37b5db1.chunk.css"
+  },
+  {
+    "revision": "2bceaaccfbdaaf620cdd",
+    "url": "/static/js/2.81fe606a.chunk.js"
+  },
+  {
+    "revision": "392df0c774d153d0213a088f648341a5",
+    "url": "/static/js/2.81fe606a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c507f77c4d05a8c752",
+    "url": "/static/js/main.f5c0b680.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
